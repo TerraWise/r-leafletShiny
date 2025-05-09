6846199040a349870214611c08a67df2f0f36340
VTM-Planfarm <vinh@planfarm.com.au>
2025-05-09 14:47:49 +0800

    Remove AWS environment variable setup and test S3 access from deploy script

--- deploy.R
+++ deploy.R
@@ -5,15 +5,6 @@ setAccountInfo(name = "terrawise",
                token = Sys.getenv("SHINY_TOKEN"),
                secret = Sys.getenv("SHINY_SECRET"))
 
-Sys.setenv(
-  "AWS_ACCESS_KEY_ID" = Sys.getenv("AWS_ACCESS_KEY_ID"),
-  "AWS_SECRET_ACCESS_KEY" = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
-  "AWS_DEFAULT_REGION" = "ap-southeast-2" # Replace with your region
-)
-
-# Test S3 access
-print(bucketlist())  # List all accessible buckets
-
-# deployApp(forceUpdate = TRUE,
-#           appFiles = c("app.R", "Data/", "www/",
-#                        "_brand.yml", "renv.lock"))
+deployApp(forceUpdate = TRUE,
+          appFiles = c("app.R", "Data/", "www/",
+                       "_brand.yml", "renv.lock"))
