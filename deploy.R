d93833b1bae953757950a9d9146baaaa522770a9
VTM-Planfarm <vinh@planfarm.com.au>
2025-05-09 14:39:38 +0800

    Test secret with a simple script

--- deploy.R
+++ deploy.R
@@ -1,12 +1,19 @@
 library(rsconnect)
+library(aws.s3)
 
 setAccountInfo(name = "terrawise",
                token = Sys.getenv("SHINY_TOKEN"),
                secret = Sys.getenv("SHINY_SECRET"))
 
-print(paste("AWS_ACCESS_KEY_ID:", Sys.getenv("AWS_ACCESS_KEY_ID")))
-print(paste("AWS_SECRET_ACCESS_KEY:", Sys.getenv("AWS_SECRET_ACCESS_KEY")))
+Sys.setenv(
+  "AWS_ACCESS_KEY_ID" = Sys.getenv("AWS_ACCESS_KEY_ID"),
+  "AWS_SECRET_ACCESS_KEY" = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
+  "AWS_DEFAULT_REGION" = "ap-southeast-2" # Replace with your region
+)
 
-deployApp(forceUpdate = TRUE,
-          appFiles = c("app.R", "Data/", "www/",
-                       "_brand.yml", "renv.lock"))
+# Test S3 access
+print(bucketlist())  # List all accessible buckets
+
+# deployApp(forceUpdate = TRUE,
+#           appFiles = c("app.R", "Data/", "www/",
+#                        "_brand.yml", "renv.lock"))
