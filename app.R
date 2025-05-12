8278a3a9a1c57395dbd0e426b67e8e7191acd092
VTM-Planfarm <vinh@planfarm.com.au>
2025-05-12 14:49:57 +0800

    Fix typo in success message for data submission

--- app.R
+++ app.R
@@ -366,7 +366,7 @@ server <- function(input, output, session) {
       showModal(
         modalDialog(
           title = "Success",
-          "Your data has been successfully sumbitted to TerraWise.",
+          "Your data has been successfully submitted to TerraWise.",
           easyClose = FALSE,
           footer = NULL
         )
